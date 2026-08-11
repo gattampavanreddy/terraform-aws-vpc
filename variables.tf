34225c1e62add72794a719d2fc1e65ba370f26b2
GattamPavan <pavanreddy2266@gmail.com>
2026-08-11 18:28:50 +0530

    actions

--- variables.tf
+++ variables.tf
@@ -6,11 +6,11 @@ variable "environment" {
     type = string
 }
 
-variable "vpc_cidr" {
+variable "vpc_cidr"{
     type = string
     default = "10.0.0/16"
-  
 }
+
 
 variable "vpc_tags" {
     type = map
