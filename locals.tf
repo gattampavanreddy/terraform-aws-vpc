3c380c164760528c0871a7ba7a06429f97b9a137
GattamPavan <pavanreddy2266@gmail.com>
2026-08-12 11:29:06 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -1,15 +1,23 @@
 locals {
-  common_tags = {
-    Project = var.project
-    Environment = var.environment
-    terraform = "true"
-  }
-   vpc_final_tags = merge(
-                local.common_tags,
-                    {
-                        Name = "${var.project}-${var.environment}"
-                    },
-                    var.vpc_tags
-                )
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        terraform = "true"
+    }
+    vpc_final_tags = merge(
+        local.common_tags,
+        {
+           Name = "${var.project}-${var.environment}"
+        },
+        var.vpc_tags
+    )
                 
+    igw_final_tags = merge(
+        local.common_tags,
+        {
+           Name = "${var.project}-${var.environment}"
+        },
+        var.igw_tags                            
+    )
+
 }
